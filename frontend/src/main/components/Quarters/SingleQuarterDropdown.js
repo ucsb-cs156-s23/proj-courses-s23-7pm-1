64075229efefd5686d2e431bec043fc58d70ca69
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-02-27 18:14:23 -0800

    pc - add SingleQuarterDropdown with full jest test coverage

---
+++ frontend/src/main/components/Quarters/SingleQuarterDropdown.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap';
+
+// controlId is used to remember the value for localStorage,
+// and for the testId, so it should be unique to at least any
+// given page where the component is used.
+
+// quarter and setQuarter should be values returned
+// by a parent component's setState 
+
+// quarters is an array of objects in this format
+// [{ yyyyq :"20214", qyy: "F21"},
+//  { yyyyq :"20221", qyy: "W22"}, 
+//  { yyyyq :"20222", qyy: "S22"}] 
+
+function SingleQuarterDropdown({ quarters, quarter, setQuarter, controlId, onChange = null, label = "Quarter" }) {
+
+    const defaultQuarter = "20212";
+
+    const localSearchQuarter = localStorage.getItem(controlId);
+
+    const [quarterState, setQuarterState] = useState(
+         localSearchQuarter || quarters[0].yyyyq
+    );
+
+    const handleQuarterOnChange = (event) => {
+        const selectedQuarter = event.target.value;
+        localStorage.setItem(controlId,selectedQuarter);
+        setQuarterState(selectedQuarter);
+        setQuarter(selectedQuarter);
+        if (onChange != null) {
+            onChange(event);
+        }
+    };
+
+    return (
+        <Form.Group controlId={controlId}>
+            <Form.Label>{label}</Form.Label>
+            <Form.Control
+                as="select"
+                value={quarterState}
+                onChange={handleQuarterOnChange}
+            >
+                {quarters.map(function (object, i) {
+                    return (
+                        <option key={controlId + '-' + i} value={object.yyyyq}>
+                            {object.qyy}
+                        </option>
+                    );
+                })}
+            </Form.Control>
+        </Form.Group>
+    );
+};
+
+export default SingleQuarterDropdown;
---
+++ frontend/src/tests/components/Quarters/SingleQuarterDropdown.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SingleQuarterDropdown from "main/components/Quarters/SingleQuarterDropdown"
+
+import { quarterRange } from 'main/utils/quarterUtilities';
+
+
+describe("SingleQuarterSelector tests", () => {
+
+    const quarter = jest.fn();
+    const setQuarter = jest.fn();
+
+    test("renders without crashing on one quarter", () => {
+        render(<SingleQuarterDropdown
+            quarters={quarterRange("20211", "20211")}
+            quarter={quarter}
+            setQuarter={setQuarter}
+            controlId="sqd1"
+        />);
+    });
+
+    test("renders without crashing on three quarters", () => {
+        render(<SingleQuarterDropdown
+            quarters={quarterRange("20214", "20222")}
+            quarter={quarter}
+            setQuarter={setQuarter}
+            controlId="sqd1"
+        />);
+    });
+
+    test("when I select an object, the value changes", () => {
+        const { getByLabelText } =
+            render(<SingleQuarterDropdown
+                quarters={quarterRange("20211", "20222")}
+                quarter={quarter}
+                setQuarter={setQuarter}
+                controlId="sqd1"
+                label="Select Quarter"
+            />
+            );
+        const selectQuarter = getByLabelText("Select Quarter")
+        userEvent.selectOptions(selectQuarter, "20213");
+        expect(setQuarter).toBeCalledWith("20213");
+    });
+
+    test("if I pass a non-null onChange, it gets called when the value changes", async () => {
+        const onChange = jest.fn();
+        const { getByLabelText } =
+            render(<SingleQuarterDropdown
+                quarters={quarterRange("20211", "20222")}
+                quarter={quarter}
+                setQuarter={setQuarter}
+                controlId="sqd1"
+                label="Select Quarter"
+                onChange={onChange}
+            />
+            );
+        const selectQuarter = getByLabelText("Select Quarter")
+        userEvent.selectOptions(selectQuarter, "20213");
+        await waitFor( ()=> expect(setQuarter).toBeCalledWith("20213") );
+        await waitFor( ()=> expect(onChange).toBeCalledTimes(1) );
+
+        // x.mock.calls[0][0] is the first argument of the first call to the jest.fn() mock x
+       
+        const event = onChange.mock.calls[0][0];
+        expect(event.target.value).toBe("20213");
+    });
+
+});
