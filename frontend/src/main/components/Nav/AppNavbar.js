a5529d0ddd473b18de3f4210c3a1f3264e350168
andrewpengucsb <56096744+andrewpengucsb@users.noreply.github.com>
2022-03-02 12:55:43 -0800

    ap - added loadsubjects place holder, navbar and tests

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -46,6 +46,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                 hasRole(currentUser, "ROLE_ADMIN") && (
                   <NavDropdown title="Admin" id="appnavbar-admin-dropdown" data-testid="appnavbar-admin-dropdown" >
                     <NavDropdown.Item href="/admin/users">Users</NavDropdown.Item>
+                    <NavDropdown.Item href="/admin/loadsubjects" data-testid="appnavbar-admin-loadsubjects">Load Subjects</NavDropdown.Item>
                   </NavDropdown>
                 )
               }
--- frontend/src/main/pages/AdminLoadSubjectsPage.js
+++ frontend/src/main/pages/AdminLoadSubjectsPage.js
@@ -1,12 +1,12 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function LoadSubjectsPage() {
+export default function AdminLoadSubjectsPage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Todos</h1>
+        <h1>Load Subjects</h1>
         <p>
-          This is where the index page will go
+          This is where the Load Subjects page will go
         </p>
       </div>
     </BasicLayout>
