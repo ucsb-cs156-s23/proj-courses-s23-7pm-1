94a2c7efea2184b87efdfa3c0ab7b10bb7d8cd71
ScottUCSB <stg@ucsb.edu>
2022-03-02 13:54:13 -0800

    sg-AdminLoadSubPAge button, try 1

--- frontend/src/main/pages/AdminLoadSubjectsPage.js
+++ frontend/src/main/pages/AdminLoadSubjectsPage.js
@@ -1,14 +1,70 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+//import UCSBSubjectForm from "main/components/UCSBSubjects/UCSBSubjectForm";
+import { Navigate } from "react-router-dom";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+import { Button } from "react-bootstrap";
 
-export default function AdminLoadSubjectsPage() {
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Load Subjects</h1>
-        <p>
-          This is where the Load Subjects page will go
-        </p>
-      </div>
-    </BasicLayout>
-  )
+
+export default function UCSBSubjectsCreatePage() {
+  const objectToAxiosParams = (ucsbSubject) => ({
+    url: "/api/ucsbsubjects/post",
+    method: "POST",
+    params: {
+      subjectCode: ucsbSubject.subjectCode,
+      subjectTranslation: ucsbSubject.subjectTranslation,
+      deptCode: ucsbSubject.deptCode,
+      collegeCode: ucsbSubject.collegeCode,
+      relatedDeptCode: ucsbSubject.relatedDeptCode,
+      inactive: ucsbSubject.inactive,
+    },
+  });
+
+  const onSuccess = (ucsbSubject) => {
+    toast(
+      `The amount of new Subjects Created : ${ucsbSubject.id}`
+    );
+  };
+  /*
+    const mutation = useBackendMutation(
+      objectToAxiosParams,
+      { onSuccess },
+      // Stryker disable next-line all : hard to set up test for caching
+      ["/api/ucsbsubjects/all"]
+    );
+  
+    const { isSuccess } = mutation;
+  
+    const onSubmit = async (data) => {
+      mutation.mutate(data);
+    };
+  
+    if (isSuccess) {
+      return <Navigate to="/ucsbsubjects/list" />;
+    }
+  */
+  function update() {
+    reload = location.reload();
+  }
+
+
+  export default function AdminLoadSubjectsPage() {
+    return (
+      <BasicLayout>
+        <div className="pt-2">
+          <h1>Load Subjects</h1>
+          <p>
+
+            <Button
+              variant='primary'
+              onClick={update()}
+              data-testid="update_Admin_LoadSubjPage"
+            >
+              Update
+            </Button>
+          </p>
+        </div>
+      </BasicLayout>
+    )
+  }
 }
