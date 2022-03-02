068166760d5705885e2c1fa256fdbec8ea8388b3
Luke Yoffe <lukeyoffe@umail.ucsb.edu>
2022-03-02 13:27:53 -0800

    fix import

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBCurriculumController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBCurriculumController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import edu.ucsb.cs156.example.UCSBCurriculumService;
+import edu.ucsb.cs156.example.services.UCSBCurriculumService;
 
 @RestController
 @RequestMapping("/api/public")
