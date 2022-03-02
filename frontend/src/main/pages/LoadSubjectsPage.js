555c77c28d56f17f614831abc312c8f8a800ce0c
andrewpengucsb <56096744+andrewpengucsb@users.noreply.github.com>
2022-03-02 12:17:14 -0800

    ap - added placeholder page for load subjects

---
+++ frontend/src/main/pages/LoadSubjectsPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function LoadSubjectsPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Todos</h1>
+        <p>
+          This is where the index page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
