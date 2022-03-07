25c63bff5d6e8e8cfbe267c7fcf72d9965d18598
Alex Lai <a_lai@ucsb.edu>
2022-03-07 13:14:01 -0800

    al - add PersonalSchedule Table

---
+++ frontend/src/fixtures/personalScheduleFixtures.js
@@ -0,0 +1,64 @@
+const personalScheduleFixtures = {
+    threePersonalSchedules:
+    [
+        {
+          "id": 1,
+          "user": {
+            "id": 1,
+            "email": "phtcon@ucsb.edu",
+            "googleSub": "115856948234298493496",
+            "pictureUrl": "https://lh3.googleusercontent.com/-bQynVrzVIrU/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmkGuVsELD1ZeV5iDUAUfe6_K-p8w/s96-c/photo.jpg",
+            "fullName": "Phill Conrad",
+            "givenName": "Phill",
+            "familyName": "Conrad",
+            "emailVerified": true,
+            "locale": "en",
+            "hostedDomain": "ucsb.edu",
+            "admin": true
+          },
+          "description": "Advanced Applications Programming",
+          "quarter": "Winter",
+          "name": "CS156"
+        },
+        {
+          "id": 2,
+          "user": {
+            "id": 1,
+            "email": "phtcon@ucsb.edu",
+            "googleSub": "115856948234298493496",
+            "pictureUrl": "https://lh3.googleusercontent.com/-bQynVrzVIrU/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmkGuVsELD1ZeV5iDUAUfe6_K-p8w/s96-c/photo.jpg",
+            "fullName": "Phill Conrad",
+            "givenName": "Phill",
+            "familyName": "Conrad",
+            "emailVerified": true,
+            "locale": "en",
+            "hostedDomain": "ucsb.edu",
+            "admin": true
+          },
+          "description": "Operating Systems",
+          "quarter": "Winter",
+          "name": "CS170"
+        },
+        {
+          "id": 3,
+          "user": {
+            "id": 1,
+            "email": "phtcon@ucsb.edu",
+            "googleSub": "115856948234298493496",
+            "pictureUrl": "https://lh3.googleusercontent.com/-bQynVrzVIrU/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmkGuVsELD1ZeV5iDUAUfe6_K-p8w/s96-c/photo.jpg",
+            "fullName": "Phill Conrad",
+            "givenName": "Phill",
+            "familyName": "Conrad",
+            "emailVerified": true,
+            "locale": "en",
+            "hostedDomain": "ucsb.edu",
+            "admin": true
+          },
+          "description": "DATA STRUCT ALGOR",
+          "quarter": "Winter",
+          "name": "CS130A"
+        }
+    ]
+};
+
+export { personalScheduleFixtures };
