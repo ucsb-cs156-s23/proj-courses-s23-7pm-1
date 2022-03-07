b1377daaaf64bcccd745c4d926614defd3641291
ScottUCSB <stg@ucsb.edu>
2022-03-07 14:58:37 -0800

    sg-Passing task 3 now, pushing 5 later

---
+++ frontend/src/main/pages/AdminLoadSubjectsPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function AdminLoadSubjectsPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Load Subjects</h1>
+        <p>
+          This is where the Load Subjects page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+} 
