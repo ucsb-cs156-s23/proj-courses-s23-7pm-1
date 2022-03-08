671809cb477d8b73be66fa4c9c5b931186f02e5f
ScottUCSB <stg@ucsb.edu>
2022-03-07 17:17:39 -0800

    sg-Missed a file that I thought I added (util for table)

---
+++ frontend/src/main/utils/UCSBSubjectUtils.js
@@ -0,0 +1,18 @@
+import { toast } from "react-toastify";
+import { useNavigate } from 'react-router-dom'
+
+export function onDeleteSuccess(message) {
+    console.log(message);
+    toast(message);
+}
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/ucsbsubjects",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
+
