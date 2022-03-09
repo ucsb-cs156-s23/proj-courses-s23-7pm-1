c747e9434c5b78ae4eb43c9fbb600250ed87c18e
ScottUCSB <stg@ucsb.edu>
2022-03-09 13:40:06 -0800

    sg-need to fix AdminLoadSubjectsPageTest still

--- frontend/src/tests/pages/AdminLoadSubjectsPage.test.js
+++ frontend/src/tests/pages/AdminLoadSubjectsPage.test.js
@@ -10,7 +10,7 @@ import AxiosMockAdapter from "axios-mock-adapter";
 
 describe("AdminLoadSubjectsPage tests", () => {
 
-    const axiosMock =new AxiosMockAdapter(axios);
+    const axiosMock = new AxiosMockAdapter(axios);
     axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
     axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
@@ -23,6 +23,49 @@ describe("AdminLoadSubjectsPage tests", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
+    });
+
+    test("Refresh button works", async () => {
+        const axiosMock = new AxiosMockAdapter(axios);
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+
+        const { getByText, getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AdminLoadSubjectsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        const editButton = getByTestId(`UCSBSubjectsTable-cell-row-0-col-Edit-button`);
+        expect(editButton).toBeInTheDocument();
+
+        fireEvent.click(editButton);
+        await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/UCSBSubjects/edit/1'));
+    });
+    test("Toast displays  properly", async () => {
+
+        const currentUser = currentUserFixtures.adminUser;
+
+        const { getByText, getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <UCSBSubjectsTable subjects={ucsbSubjectsFixtures.threeSubjects} currentUser={currentUser} />
+                </MemoryRouter>
+            </QueryClientProvider>
+
+        );
+
+        await waitFor(() => { expect(getByTestId(`UCSBSubjectsTable-cell-row-0-col-id`)).toHaveTextContent("1"); });
+
+        const editButton = getByTestId(`UCSBSubjectsTable-cell-row-0-col-Edit-button`);
+        expect(editButton).toBeInTheDocument();
+
+        fireEvent.click(editButton);
+
+        await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/UCSBSubjects/edit/1'));
+
     });
 
 
