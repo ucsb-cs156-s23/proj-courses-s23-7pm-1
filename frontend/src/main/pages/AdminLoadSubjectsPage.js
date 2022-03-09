99acc2d7a550bb82544c668c169ef20da03eda58
ScottUCSB <stg@ucsb.edu>
2022-03-09 13:11:58 -0800

    sg-trying out older setup, need to figure out how to find the delta

--- frontend/src/main/pages/AdminLoadSubjectsPage.js
+++ frontend/src/main/pages/AdminLoadSubjectsPage.js
@@ -1,36 +1,29 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import { Navigate } from "react-router-dom";
 import { useBackendMutation, useBackend } from "main/utils/useBackend";
 import { toast } from "react-toastify";
 import { Button } from "react-bootstrap";
 import UCSBSubjectsTable from 'main/components/UCSBSubjects/UCSBSubjectsTable';
 import { useCurrentUser } from 'main/utils/currentUser'
 
-export default function AdminLoadSubjectsPage() {                     //NOT DONE
+export default function AdminLoadSubjectsPage() {                     
+  
+  const currentUser = useCurrentUser();
+  const { data: subjects, error: _error, status: _status } =
+      useBackend(
+        // Stryker disable next-line all : don't test internal caching of React Query
+        ["/api/UCSBSubjects/all"], { method: "GET", url: "/api/UCSBSubjects/all" }, []
+      );
+
   const objectToAxiosParams = () => ({
-    url: "/api/UCSBSubjects/post",
+    url: "/api/UCSBSubjects/load",
     method: "POST",
   });
 
 
-//delete things marked with this [***] after testing it out
-//[***] 
-
-const currentUser = useCurrentUser();
-const { data: subjects, error: _error, status: _status } =
-    useBackend(
-      // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/UCSBSubjects/all"],
-      { method: "GET", url: "/api/UCSBSubjects/all" },
-      []
-    );
-
-    //[***]
-
-
   const onSuccess = () => {
+    //Todo: Toast should say how much new subjects were loaded in, not just number of subjects loaded in
     toast(
-      `The amount of new Subjects Created : ${ucsbSubject.length}`
+      `Number of Subjects Loaded : ${subjects.length}`
     );
   }
 
@@ -38,32 +31,95 @@ const { data: subjects, error: _error, status: _status } =
     objectToAxiosParams,
     { onSuccess },
     // Stryker disable next-line all : hard to set up test for caching
-    ["/api/UCSBSubjects/retrieve"]
+    ["/api/UCSBSubjects/all"]
   );
 
-//if(ucsbSubject.length>0){
   
-
-
-  const { isSuccess } = mutation
 
   const onSubmit = async () => {
     mutation.mutate();
   }
 
-  if (isSuccess) {
-    window.location.reload();
-    //return <Navigate to="/admin/loadsubjects" />
-  }
 
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Load Subjects</h1>
   
-        <button onclick="onSubmit()">Refresh the Table</button>
+        <Button variant = 'primary' onClick={onSubmit}>Refresh the Table</Button>
+        <UCSBSubjectsTable subjects={subjects} currentUser={currentUser} />
+
 
       </div>
     </BasicLayout>
   );
 }
+
+
+
+
+/*                                                   //trying out these
+
+
+<UCSBSubjectsTable subjects={subjects} currentUser={currentUser} />
+
+
+//works but with table the page disapears after a second
+<input type="button" value="Refresh" onclick="onSubmit()" />
+
+
+const mutation = useBackendMutation(
+        objectToAxiosParams,
+        { onSuccess },
+        // Stryker disable next-line all : hard to set up test for caching
+        ["/api/ucsbsubjects/all"]
+      );
+    
+      const { isSuccess } = mutation;
+    
+      const onSubmit = async (data) => {
+        mutation.mutate(data);
+      };
+    
+      if (isSuccess) {
+        return <Navigate to="/ucsbsubjects/list" />;
+      }
+   
+    function update() {
+      reload = location.reload();
+    }
+    //get new list, get old - find delta. could loop through new set and see if in table if not
+   */
+
+/*
+
+ <Button
+          variant='primary'
+          onClick={onSubmit()}
+          data-testid="update_Admin_LoadSubjPage"
+        >
+          Update
+        </Button>
+
+
+
+
+
+        <input type="button" value="Refresh" onclick="history.go(0)" />
+
+<submitAction={onSubmit} />
+
+return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Load Subjects</h1>
+        <p>
+          This is where the Load Subjects page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
+
+
+*/
