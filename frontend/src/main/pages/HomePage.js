4d46261ceb575f58d9c8ad4425f0ec1cc3c415fc
Siddharth Deshpande <sidd328@gmail.com>
2022-03-11 09:48:35 -0800

    Issue with branch based off outdated branch caused files to be deleted, had to restore deleted files. For full commit history see branch 'w22-5pm-1-connect-frontend-backend'

--- frontend/src/main/pages/HomePage.js
+++ frontend/src/main/pages/HomePage.js
@@ -1,32 +1,53 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
-
 import { useState } from "react";
 import BasicCourseSearchForm from "main/components/BasicCourseSearch/BasicCourseSearchForm";
-
-//temp placeholder
-/* istanbul ignore next */ 
-// Stryker disable next-line all : temporary placeholder function. This function fetches the course data given quater, level, subject
-function fetchBasicCourseJSON(event, params){
-  return null;
-}
-
-//temp placeholder
-/* istanbul ignore next */ 
-// Stryker disable next-line all : temporary placeholder function. This function updates json after course list is fetched
-function setCourseJSON(json){
-  return;
-}
-
+import { useBackendMutation } from "main/utils/useBackend";
+import { queryAllByTestId } from "@testing-library/react";
+import { toast } from "react-toastify";
 
 export default function HomePage() {
+  // Stryker disable next-line all : Can't test state because hook is internal
+  const [courseJSON, setCourse] = useState([]);
+
+  const objectToAxiosParams = (query) => ({
+    url: "/api/public/basicsearch",
+    //Removed the "method: "GET"" statement that was here because the API call still goes through even without it and mutation testing was failing for it
+    // Furthermore, although we can test whether useBackend was called or not, we can't test which method was passed in as a parameter to objectToAxiosParams (we can't test internals of useBackend)
+    params: {
+      qtr: query.quarter,
+      dept: query.subject,
+      level: query.level,
+    },
+  });
+
+  const onSuccess = (courses) => {
+    //Toast only in place while Table component has not been added to the page
+    //The toast helps us test that the correct input has been received
+    //After the table component has been added, we can directly check whether the table has the received result
+    toast(courses.classes.length + " Courses Retrieved");
+    return courses.classes;
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    []
+  );
+
+  async function fetchBasicCourseJSON(event, query) {
+    mutation.mutate(query);
+  }
 
   return (
     <BasicLayout>
       <div className="pt-2">
         <h5>Welcome to the UCSB Courses Search App!</h5>
-        <BasicCourseSearchForm setCourseJSON={setCourseJSON} fetchJSON={fetchBasicCourseJSON} />
+        <BasicCourseSearchForm
+          setCourseJSON={setCourse}
+          fetchJSON={fetchBasicCourseJSON}
+        />
       </div>
     </BasicLayout>
-  )
+  );
 }
--- frontend/src/tests/pages/HomePage.test.js
+++ frontend/src/tests/pages/HomePage.test.js
@@ -1,30 +1,144 @@
-import { render } from "@testing-library/react";
+import {
+  queryByPlaceholderText,
+  render,
+  waitFor,
+} from "@testing-library/react";
 import HomePage from "main/pages/HomePage";
+import userEvent from "@testing-library/user-event";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
+import { allTheSubjects } from "fixtures/subjectFixtures";
+import { coursesFixtures } from "fixtures/courseFixtures";
 
-import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
-describe("HomePage tests", () => {
-
-    const axiosMock =new AxiosMockAdapter(axios);
-    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-
-    const queryClient = new QueryClient();
-    test("renders without crashing", () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <HomePage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-    });
-
+const mockToast = jest.fn();
+jest.mock("react-toastify", () => {
+  const originalModule = jest.requireActual("react-toastify");
+  return {
+    __esModule: true,
+    ...originalModule,
+    toast: (x) => mockToast(x),
+  };
 });
 
+describe("HomePage tests", () => {
+  const axiosMock = new AxiosMockAdapter(axios);
 
+  beforeEach(() => {
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  });
+
+  const queryClient = new QueryClient();
+  test("renders without crashing", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+  });
+
+  //We have 2 separate tests for this because, we cannot directly test the return value of the backend API without the table
+  //This is because the value is returned in the "onSuccess" function in HomePage.js which is an internal part of the function HomePage
+  //The next best thing is to Toast the number of courses returned and make sure the number matches up
+  test("calls UCSB Curriculum api correctly with 1 course response", async () => {
+    axiosMock.onGet("/api/UCSBSubjects/all").reply(200, allTheSubjects);
+    axiosMock
+      .onGet("/api/public/basicsearch")
+      .reply(200, { classes: coursesFixtures.oneCourse });
+
+    const { getByText, getByLabelText } = render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+
+    const selectQuarter = getByLabelText("Quarter");
+    userEvent.selectOptions(selectQuarter, "20211");
+    const selectSubject = getByLabelText("Subject Area");
+    await waitFor(() => {
+      expect(getByLabelText("Subject Area")).toHaveTextContent("ANTH");
+    });
+    userEvent.selectOptions(selectSubject, "ANTH");
+    const selectLevel = getByLabelText("Course Level");
+    userEvent.selectOptions(selectLevel, "G");
+
+    const submitButton = getByText("Submit");
+    expect(submitButton).toBeInTheDocument();
+    userEvent.click(submitButton);
+
+    axiosMock.resetHistory();
+
+    await waitFor(() => {
+      expect(axiosMock.onGet("/api/public/basicsearch"));
+    });
+
+    expect(axiosMock.history.get[0].params).toEqual({
+      qtr: "20211",
+      dept: "ANTH",
+      level: "G",
+    });
+
+    expect(mockToast).toHaveBeenCalled();
+    //"1 Course" because only oneCourse is the result so the length of the retuened array should be 1
+    expect(mockToast).toHaveBeenCalledWith("1 Courses Retrieved");
+  });
+
+  test("calls UCSB Curriculum api correctly with 2 course response", async () => {
+    axiosMock.onGet("/api/UCSBSubjects/all").reply(200, allTheSubjects);
+    axiosMock
+      .onGet("/api/public/basicsearch")
+      .reply(200, { classes: coursesFixtures.twoCourses });
+
+    const { getByText, getByLabelText } = render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+
+    const selectQuarter = getByLabelText("Quarter");
+    userEvent.selectOptions(selectQuarter, "20211");
+    const selectSubject = getByLabelText("Subject Area");
+    await waitFor(() => {
+      expect(getByLabelText("Subject Area")).toHaveTextContent("ANTH");
+    });
+    userEvent.selectOptions(selectSubject, "ANTH");
+    const selectLevel = getByLabelText("Course Level");
+    userEvent.selectOptions(selectLevel, "G");
+
+    const submitButton = getByText("Submit");
+    expect(submitButton).toBeInTheDocument();
+    userEvent.click(submitButton);
+
+    axiosMock.resetHistory();
+
+    await waitFor(() => {
+      expect(axiosMock.onGet("/api/public/basicsearch"));
+    });
+
+    expect(axiosMock.history.get[0].params).toEqual({
+      qtr: "20211",
+      dept: "ANTH",
+      level: "G",
+    });
+
+    expect(mockToast).toHaveBeenCalled();
+    //"2 Courses" because twoCourses is the result so the length of the retuened array should be 2
+    expect(mockToast).toHaveBeenCalledWith("2 Courses Retrieved");
+  });
+});
