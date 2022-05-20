3d99b245f376bc8105f4a16f60e31b5b8f558dc7
andrewpengucsb <56096744+andrewpengucsb@users.noreply.github.com>
2022-05-20 16:01:20 -0700

    ap-change missed lines/files from examples to courses

--- src/main/java/edu/ucsb/cs156/courses/CoursesApplication.java
+++ src/main/java/edu/ucsb/cs156/courses/CoursesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class CoursesApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ExampleApplication.class, args);
+    SpringApplication.run(CoursesApplication.class, args);
   }
 
 }
