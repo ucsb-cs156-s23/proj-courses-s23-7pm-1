2adc945c9e50633b7142c3dbe3c32638b77dcf41
Morgan6688 <shu-hsien@ucsb.edu>
2022-05-25 17:40:46 -0700

    ml - added entities and repositories for courses

---
+++ src/main/java/edu/ucsb/cs156/courses/entities/Courses.java
@@ -0,0 +1,27 @@
+package edu.ucsb.cs156.courses.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "courses")
+public class Courses {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String enrollCd;
+  private String psld;
+  private String quarter;
+}
