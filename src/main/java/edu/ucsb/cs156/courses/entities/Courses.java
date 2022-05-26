59ce66ad465a3eadb54eccd44e1564057c747a58
Morgan6688 <shu-hsien@ucsb.edu>
2022-05-26 15:08:13 -0700

    ml - added controllers for courses, swagger works for users but might need to be changed

--- src/main/java/edu/ucsb/cs156/courses/entities/Courses.java
+++ src/main/java/edu/ucsb/cs156/courses/entities/Courses.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 
 import lombok.Data;
@@ -21,6 +23,9 @@ public class Courses {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
   private String enrollCd;
   private String psId;
   private String quarter;
