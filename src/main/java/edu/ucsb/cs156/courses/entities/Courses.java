6c2ec38210867783eef5cd34b7c49b58ce17bc8f
Morgan6688 <shu-hsien@ucsb.edu>
2022-05-31 17:47:22 -0700

    ml - added course controllers and tests

--- src/main/java/edu/ucsb/cs156/courses/entities/Courses.java
+++ src/main/java/edu/ucsb/cs156/courses/entities/Courses.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 
 import lombok.Data;
@@ -21,7 +23,10 @@ public class Courses {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
   private String enrollCd;
-  private String psId;
+  private long psId;
   private String quarter;
 }
