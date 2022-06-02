b4dc902785cc75c3defcecde4638e683798a61be
Aviv Samet <aviv@ucsb.edu>
2022-06-01 22:07:10 -0700

    fixed footer tests

--- frontend/src/tests/components/Nav/Footer.test.js
+++ frontend/src/tests/components/Nav/Footer.test.js
@@ -12,18 +12,28 @@ describe("Footer tests", () => {
       });
     
       test("Links are correct", async () => {
-        const { getByText } = render(<Footer />);
-        expect(getByText("CMPSC 156").closest("a")).toHaveAttribute(
+        render(<Footer />)
+        expect(screen.getByTestId("footer-class-website-link")).toHaveAttribute(
           "href",
           "https://ucsb-cs156.github.io"
         );
-        expect(getByText("UCSB").closest("a")).toHaveAttribute(
+        expect(screen.getByTestId("footer-ucsb-link")).toHaveAttribute(
           "href",
           "https://ucsb.edu"
         );
-        expect(getByText(/GitHub/i).closest("a")).toHaveAttribute(
+        expect(screen.getByTestId("footer-source-code-link")).toHaveAttribute(
           "href",
           "https://github.com/ucsb-cs156-s22/s22-4pm-courses"
         );
+
+        expect(screen.getByTestId("footer-sticker-link")).toHaveAttribute(
+            "href",
+            "https://www.as.ucsb.edu/sticker-packs/>"
+          );
+          expect(screen.getByTestId("footer-course-search-link")).toHaveAttribute(
+            "href",
+            "https://my.sa.ucsb.edu/public/curriculum/coursesearch.aspx"
+          );
+
       });
 });
