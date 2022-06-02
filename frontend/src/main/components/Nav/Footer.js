b95424f63b187d5b8e8dcd057f7000283eb23adb
Aviv Samet <aviv@ucsb.edu>
2022-06-01 18:45:38 -0700

    added icon to navbar and favicon to browser tab. localhost:8080 does not display the image correctly

--- frontend/src/main/components/Nav/Footer.js
+++ frontend/src/main/components/Nav/Footer.js
@@ -1,5 +1,7 @@
 import { Container } from "react-bootstrap";
 
+export const space=" ";
+
 export default function Footer() {
   return (
     <footer className="bg-light pt-3 pt-md-4 pb-4 pb-md-5">
