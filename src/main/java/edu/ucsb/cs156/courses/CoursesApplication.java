4d209d388328805bd6a2a949ebc6eb786f45014e
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-11-16 10:24:18 -0800

    pc - add backend for async job system
    This adds the ability to submit asynchronous jobs in the backend,
    including one demonstration test job.  Tests are included as well.

--- src/main/java/edu/ucsb/cs156/courses/CoursesApplication.java
+++ src/main/java/edu/ucsb/cs156/courses/CoursesApplication.java
@@ -3,7 +3,11 @@ package edu.ucsb.cs156.courses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.scheduling.annotation.EnableAsync;
+
 @SpringBootApplication
+@EnableAsync
 public class CoursesApplication {
 
   public static void main(String[] args) {
