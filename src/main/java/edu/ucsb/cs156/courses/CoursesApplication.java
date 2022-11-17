4c63a13cb9539f3936398587edf354614321909d
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-11-17 09:42:46 -0800

    pc - fix time zone issue with createdAt and updatedAt dates
    We need to both generate and store these dates with their
    time zone, so that they get reported consistently regardless
    of whether the server's local time is Pacific, Eastern, GMT or whatever

--- src/main/java/edu/ucsb/cs156/courses/CoursesApplication.java
+++ src/main/java/edu/ucsb/cs156/courses/CoursesApplication.java
@@ -1,12 +1,22 @@
 package edu.ucsb.cs156.courses;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
-@EnableJpaAuditing // enables automatic population of @CreatedDate and @LastModifiedDate
+@EnableJpaAuditing(dateTimeProviderRef = "utcDateTimeProvider")
+// enables automatic population of @CreatedDate and @LastModifiedDate
 @EnableAsync // for @Async annotation for JobsService
 public class CoursesApplication {
 
@@ -14,4 +24,11 @@ public class CoursesApplication {
     SpringApplication.run(CoursesApplication.class, args);
   }
 
+  @Bean
+    public DateTimeProvider utcDateTimeProvider() {
+        return () -> {
+          ZonedDateTime now = ZonedDateTime.now();
+          return Optional.of(now);
+        };
+    }
 }
