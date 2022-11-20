cbd82c35ed497c6b2e6ec675ca85015886fb8492
glt <glt@mail2tor.com>
2022-11-20 15:49:42 -0800

    glt - Add more SectionFixtures entries
    We need at a few more example fixtures in order to test the new POST
    /api/courses/post properly.

--- src/test/java/edu/ucsb/cs156/courses/documents/SectionFixtures.java
+++ src/test/java/edu/ucsb/cs156/courses/documents/SectionFixtures.java
@@ -56,10 +56,338 @@ public class SectionFixtures {
               "functionCode": "Teaching and in charge"
             }
           ]
-        }
+		}
       ]
     }
       """;
+  public static final String SECTION_JSON_CMPSC100 = """ 
+    {
+      "quarter": "20221",
+      "courseId": "CMPSC   100  ",
+      "title": "INTRO TEACH METHODS",
+      "contactHours": 30,
+      "description": "Designed to train outstanding undergraduates for learning assistant positio ns in CS courses. Lecture/discussion surveys current research and best prac tices in CS pedagogy including student development theories, different peda gogical techniques, and methods for assessing learning. Students gain exper ience working one-on-one with students, fostering positive learning environ ments, and providing feedback on student work. Students who successfully co mplete this course will earn units by serving as an apprentice undergraduat e learning assistant.",
+      "college": "ENGR",
+      "objLevelCode": "U",
+      "subjectArea": "CMPSC   ",
+      "unitsFixed": 4,
+      "unitsVariableHigh": null,
+      "unitsVariableLow": null,
+      "delayedSectioning": null,
+      "inProgressCourse": null,
+      "gradingOption": "L",
+      "instructionType": "LEC",
+      "onLineCourse": false,
+      "deptCode": "CMPSC",
+      "generalEducation": [],
+      "classSections": [
+        {
+          "enrollCode": "63388",
+          "section": "0101",
+          "session": null,
+          "classClosed": null,
+          "courseCancelled": null,
+          "gradingOptionCode": null,
+          "enrolledTotal": 18,
+          "maxEnroll": 25,
+          "secondaryStatus": null,
+          "departmentApprovalRequired": false,
+          "instructorApprovalRequired": true,
+          "restrictionLevel": null,
+          "restrictionMajor": null,
+          "restrictionMajorPass": null,
+          "restrictionMinor": null,
+          "restrictionMinorPass": null,
+          "concurrentCourses": [],
+          "timeLocations": [
+            {
+              "room": "3525",
+              "building": "PHELP",
+              "roomCapacity": null,
+              "days": "  W    ",
+              "beginTime": "09:00",
+              "endTime": "11:50"
+            }
+          ],
+          "instructors": [
+            {
+              "instructor": "MIRZA D",
+              "functionCode": "Teaching and in charge"
+            }
+          ]
+	},
+	{
+          "enrollCode": "63370",
+          "section": "0100",
+          "session": null,
+          "classClosed": null,
+          "courseCancelled": null,
+          "gradingOptionCode": null,
+          "enrolledTotal": 18,
+          "maxEnroll": 25,
+          "secondaryStatus": "R",
+          "departmentApprovalRequired": false,
+          "instructorApprovalRequired": true,
+          "restrictionLevel": null,
+          "restrictionMajor": null,
+          "restrictionMajorPass": null,
+          "restrictionMinor": null,
+          "restrictionMinorPass": null,
+          "concurrentCourses": [],
+          "timeLocations": [
+            {
+              "room": "3526",
+              "building": "PHELP",
+              "roomCapacity": null,
+              "days": "    F  ",
+              "beginTime": "12:30",
+              "endTime": "13:45"
+            }
+          ],
+          "instructors": [
+            {
+              "instructor": "MIRZA D",
+              "functionCode": "Teaching and in charge"
+            }
+          ]
+        }
+      ]
+    }
+          """;
+  public static final String SECTION_JSON_CMPSC156 = """
+    {
+      "quarter": "20221",
+      "courseId": "CMPSC   156  ",
+      "title": "ADV APP PROGRAM",
+      "contactHours": 30,
+      "description": "Advanced application programming using a high-level, virtual-machine-based language. Topics include generic programming, exception handling, automatic memory management, and application development, management, and maintenanc e tools, third-party library use, version control, software testing, issue tracking, code review, and working with legacy code.",
+      "college": "ENGR",
+      "objLevelCode": "U",
+      "subjectArea": "CMPSC   ",
+      "unitsFixed": 4,
+      "unitsVariableHigh": null,
+      "unitsVariableLow": null,
+      "delayedSectioning": null,
+      "inProgressCourse": null,
+      "gradingOption": "L",
+      "instructionType": "LEC",
+      "onLineCourse": false,
+      "deptCode": "CMPSC",
+      "generalEducation": [],
+      "classSections": [
+        {
+          "enrollCode": "08292",
+          "section": "0100",
+          "session": null,
+          "classClosed": "Y",
+          "courseCancelled": null,
+          "gradingOptionCode": null,
+          "enrolledTotal": 72,
+          "maxEnroll": 72,
+          "secondaryStatus": "R",
+          "departmentApprovalRequired": false,
+          "instructorApprovalRequired": false,
+          "restrictionLevel": null,
+          "restrictionMajor": "+CMPSC+CMPEN+CPSCI",
+          "restrictionMajorPass": null,
+          "restrictionMinor": null,
+          "restrictionMinorPass": null,
+          "concurrentCourses": [],
+          "timeLocations": [
+            {
+              "room": "1431",
+              "building": "SH",
+              "roomCapacity": 76,
+              "days": "M W    ",
+              "beginTime": "12:30",
+              "endTime": "13:45"
+            }
+          ],
+          "instructors": [
+            {
+              "instructor": "CONRAD P T",
+              "functionCode": "Teaching and in charge"
+            }
+          ]
+        },
+        {
+          "enrollCode": "08300",
+          "section": "0101",
+          "session": null,
+          "classClosed": null,
+          "courseCancelled": null,
+          "gradingOptionCode": null,
+          "enrolledTotal": 24,
+          "maxEnroll": 24,
+          "secondaryStatus": null,
+          "departmentApprovalRequired": false,
+          "instructorApprovalRequired": false,
+          "restrictionLevel": null,
+          "restrictionMajor": "+CMPSC+CMPEN+CPSCI",
+          "restrictionMajorPass": null,
+          "restrictionMinor": null,
+          "restrictionMinorPass": null,
+          "concurrentCourses": [],
+          "timeLocations": [
+            {
+              "room": "3525",
+              "building": "PHELP",
+              "roomCapacity": null,
+              "days": " T     ",
+              "beginTime": "17:00",
+              "endTime": "17:50"
+            }
+          ],
+          "instructors": [
+            {
+              "instructor": "LU A H",
+              "functionCode": "Teaching but not in charge"
+            },
+            {
+              "instructor": "HEFFERNAN K J",
+              "functionCode": "Teaching but not in charge"
+            }
+          ]
+        },
+        {
+          "enrollCode": "08318",
+          "section": "0102",
+          "session": null,
+          "classClosed": null,
+          "courseCancelled": null,
+          "gradingOptionCode": null,
+          "enrolledTotal": 24,
+          "maxEnroll": 24,
+          "secondaryStatus": null,
+          "departmentApprovalRequired": false,
+          "instructorApprovalRequired": false,
+          "restrictionLevel": null,
+          "restrictionMajor": "+CMPSC+CMPEN+CPSCI",
+          "restrictionMajorPass": null,
+          "restrictionMinor": null,
+          "restrictionMinorPass": null,
+          "concurrentCourses": [],
+          "timeLocations": [
+            {
+              "room": "3525",
+              "building": "PHELP",
+              "roomCapacity": null,
+              "days": " T     ",
+              "beginTime": "18:00",
+              "endTime": "18:50"
+            }
+          ],
+          "instructors": [
+            {
+              "instructor": "HEFFERNAN K J",
+              "functionCode": "Teaching but not in charge"
+            },
+            {
+              "instructor": "LU A H",
+              "functionCode": "Teaching but not in charge"
+            }
+          ]
+        },
+        {
+          "enrollCode": "08326",
+          "section": "0103",
+          "session": null,
+          "classClosed": null,
+          "courseCancelled": null,
+          "gradingOptionCode": null,
+          "enrolledTotal": 24,
+          "maxEnroll": 24,
+          "secondaryStatus": null,
+          "departmentApprovalRequired": false,
+          "instructorApprovalRequired": false,
+          "restrictionLevel": null,
+          "restrictionMajor": "+CMPSC+CMPEN+CPSCI",
+          "restrictionMajorPass": null,
+          "restrictionMinor": null,
+          "restrictionMinorPass": null,
+          "concurrentCourses": [],
+          "timeLocations": [
+            {
+              "room": "3525",
+              "building": "PHELP",
+              "roomCapacity": null,
+              "days": " T     ",
+              "beginTime": "19:00",
+              "endTime": "19:50"
+            }
+          ],
+          "instructors": [
+            {
+              "instructor": "LU A H",
+              "functionCode": "Teaching but not in charge"
+            },
+            {
+              "instructor": "HEFFERNAN K J",
+              "functionCode": "Teaching but not in charge"
+            }
+          ]
+        }
+      ]
+    }
+    """;
 
+  public static final String SECTION_JSON_CMPSC291A = """
+    {
+      "quarter": "20221",
+      "courseId": "CMPSC   291A ",
+      "title": "SPEC TOPIC APPS GEN",
+      "contactHours": 30,
+      "description": "These courses provide for the study of topics of current   interest in computer science applications.",
+      "college": "ENGR",
+      "objLevelCode": "G",
+      "subjectArea": "CMPSC   ",
+      "unitsFixed": 4,
+      "unitsVariableHigh": null,
+      "unitsVariableLow": null,
+      "delayedSectioning": null,
+      "inProgressCourse": null,
+      "gradingOption": "L",
+      "instructionType": "LEC",
+      "onLineCourse": false,
+      "deptCode": "CMPSC",
+      "generalEducation": [],
+      "classSections": [
+        {
+          "enrollCode": "08896",
+          "section": "0200",
+          "session": null,
+          "classClosed": "Y",
+          "courseCancelled": null,
+          "gradingOptionCode": null,
+          "enrolledTotal": 5,
+          "maxEnroll": 10,
+          "secondaryStatus": null,
+          "departmentApprovalRequired": false,
+          "instructorApprovalRequired": false,
+          "restrictionLevel": "G",
+          "restrictionMajor": "+CMPSC",
+          "restrictionMajorPass": null,
+          "restrictionMinor": null,
+          "restrictionMinorPass": null,
+          "concurrentCourses": [],
+          "timeLocations": [
+            {
+              "room": "2510",
+              "building": "PHELP",
+              "roomCapacity": null,
+              "days": "M W    ",
+              "beginTime": "13:00",
+              "endTime": "14:50"
+            }
+          ],
+          "instructors": [
+            {
+              "instructor": "YAN L",
+              "functionCode": "Teaching and in charge"
+            }
+          ]
+        }
+      ]
+    }
+    """;
 }
-  
