7ec782685bc308801e93b462982fedfef61b27ea
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-11-21 16:25:14 -0800

    pc - exclude value of key from mutation testing
    It is internal to React and not exposed, so it cannot be tested

--- frontend/src/main/components/Utils/Plaintext.js
+++ frontend/src/main/components/Utils/Plaintext.js
@@ -2,17 +2,18 @@
 
 import PlaintextLine from "./PlaintextLine";
 
-export default function Plaintext({text}) {
-  if (text==null) {
+export default function Plaintext({ text }) {
+  if (text == null) {
     return (<pre data-testid="plaintext-empty"></pre>)
   }
   const textToRender = typeof text === "string" ? text : JSON.stringify(text, null, 2);
   const [firstLine, ...rest] = textToRender.split('\n')
   return (
     <pre data-testid="plaintext">
-      <span key={"span-0"}>{ firstLine }</span>
+      <span key={"0"}>{firstLine}</span>
       {
-        rest.map((line,index) => <PlaintextLine key={index+1} text={line} />)
+        // Stryker disable next-line ArithmeticOperator : key value is internal to React and not exposed to tests
+        rest.map((line, index) => <PlaintextLine key={index + 1} text={line} />)
       }
     </pre>
   );
