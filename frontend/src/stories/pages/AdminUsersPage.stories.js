5df0bf3e15ed13337cb417d5ab4cb99c14c60758
Phill Conrad <phtcon@ucsb.edu>
2023-05-10 13:18:42 -0700

    pc - add story for AdminUsersPage

---
+++ frontend/src/stories/pages/AdminUsersPage.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AdminUsersPage from "main/pages/AdminUsersPage";
+
+export default {
+    title: 'pages/AdminUsersPage',
+    component: AdminUsersPage
+};
+
+const Template = () => <AdminUsersPage />;
+
+export const Default = Template.bind({});
+
