92b444f075b98023f75983b13398907c87aa2aae
Hinojosa, Ricardo <rhinojosa1@go.pasadenua.edu>
2023-05-23 22:57:22 -0700

    rh - added entity and repository files for Course Grade History

---
+++ src/main/java/edu/ucsb/cs156/courses/entities/CourseGradeHistory.java
@@ -0,0 +1,32 @@
+package edu.ucsb.cs156.courses.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "course_grade_history")
+public class CourseGradeHistory {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String quarter;
+  private String courseLevel;
+  private String course;
+  private String instructor;
+  private String gradeGiven;
+  private int sumOfStudentsByGradeGiven;
+}
