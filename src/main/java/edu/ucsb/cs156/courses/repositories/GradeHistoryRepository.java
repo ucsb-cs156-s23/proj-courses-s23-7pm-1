582abeda7c6613acf8aa5defe2b0786b7ff62364
Hinojosa, Ricardo <rhinojosa1@go.pasadenua.edu>
2023-05-27 00:06:28 -0700

    rh - removed reposotory methods

--- src/main/java/edu/ucsb/cs156/courses/repositories/GradeHistoryRepository.java
+++ src/main/java/edu/ucsb/cs156/courses/repositories/GradeHistoryRepository.java
@@ -11,27 +11,5 @@ package edu.ucsb.cs156.courses.repositories;
 
  @Repository
  public interface GradeHistoryRepository extends CrudRepository<GradeHistory, Long> {
-     public List<GradeHistory> findByYearAndQuarterAndSubjectAreaAndCourseAndInstructorAndGrade(String year,
-             String quarter, String subjectArea, String course, String instructor, String grade);
 
-     public List<GradeHistory> findBySubjectAreaAndCourse(String subjectArea, String course);
-
-     public default List<GradeHistory> upsertAll(List<GradeHistory> gradeHistories) {
-         List<GradeHistory> result = new ArrayList<GradeHistory>();
-         for (GradeHistory gradeHistory : gradeHistories) {
-             List<GradeHistory> query = findByYearAndQuarterAndSubjectAreaAndCourseAndInstructorAndGrade(
-                     gradeHistory.getYear(), gradeHistory.getQuarter(), gradeHistory.getSubjectArea(),
-                     gradeHistory.getCourse(), gradeHistory.getInstructor(), gradeHistory.getGrade());
-             if (query.size() == 0) {
-                 gradeHistory = save(gradeHistory);
-                 result.add(gradeHistory);
-             } else {
-                 GradeHistory existing = query.get(0);
-                 existing.setCount(gradeHistory.getCount());
-                 existing = save(existing);
-                 result.add(existing);
-             }
-         }
-         return result;
-     }
  }
