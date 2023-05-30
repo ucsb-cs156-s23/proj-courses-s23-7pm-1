4e467de1624ec3849bf3053beb5c4e97af9c168b
Benjamin Cruttenden <cruttenden@cs.ucsb.edu>
2023-05-30 14:55:47 -0700

    bc - added stories for instructor search form and index page

---
+++ frontend/src/stories/components/BasicCourseSearch/InstructorCourseSearchForm.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import InstructorCourseSearchForm from "main/components/InstructorCourseSearch/InstructorCourseSearchForm";
+import { allTheSubjects } from "fixtures/subjectFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+
+export default {
+  title: "components/InstructorCourseSearch/InstructorCourseSearchForm",
+  component: InstructorCourseSearchForm,
+  parameters: {
+    mockData: [
+      {
+        url: '/api/UCSBSubjects/all',
+        method: 'GET',
+        status: 200,
+        response: allTheSubjects
+      },
+      {
+        url: '/api/systemInfo',
+        method: 'GET',
+        status: 200,
+        response: systemInfoFixtures.showingBothStartAndEndQtr
+      },
+    ],
+  },
+};
+
+const Template = (args) => {
+  return <InstructorCourseSearchForm {...args} />;
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  submitText: "Create",
+  fetchJSON: (_event, data) => {
+    console.log("Submit was clicked, data=", data);
+  }
+};
