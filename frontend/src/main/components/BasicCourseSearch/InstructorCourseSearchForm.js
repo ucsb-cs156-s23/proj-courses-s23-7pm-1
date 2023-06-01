0b4ed72f43454934ad7f092d819f69b5ca688440
Benjamin Cruttenden <cruttenden@cs.ucsb.edu>
2023-05-31 20:46:37 -0700

    bc-got rid of commented out code, changed navbar to reflect other issue to not cause merge conflicts

--- frontend/src/main/components/BasicCourseSearch/InstructorCourseSearchForm.js
+++ frontend/src/main/components/BasicCourseSearch/InstructorCourseSearchForm.js
@@ -1,13 +1,9 @@
 import { useState } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
-
-//import { allTheLevels } from "fixtures/levelsFixtures";
 import { quarterRange } from "main/utils/quarterUtilities";
 
 import { useSystemInfo } from "main/utils/systemInfo";
 import SingleQuarterDropdown from "../Quarters/SingleQuarterDropdown";
-//import SingleSubjectDropdown from "../Subjects/SingleSubjectDropdown";
-//import SingleLevelDropdown from "../Levels/SingleLevelDropdown";
 import { useBackend  } from "main/utils/useBackend";
 
 const InstructorCourseSearchForm = ({ fetchJSON }) => {
@@ -24,8 +20,6 @@ const InstructorCourseSearchForm = ({ fetchJSON }) => {
   // Stryker disable all : not sure how to test/mock local storage
   const localStartQuarter = localStorage.getItem("InstructorCourseSearch.StartQuarter");
   const localEndQuarter = localStorage.getItem("InstructorCourseSearch.EndQuarter");
-  //const localSubject = localStorage.getItem("InstructorCourseSearch.Subject");
-  //const localCourseNumber = localStorage.getItem("InstructorCourseSearch.CourseNumber");
   const localInstructor = localStorage.getItem("InstructorCourseSearch.instructor");
 
   const { error: _error, status: _status } =
@@ -38,19 +32,12 @@ const InstructorCourseSearchForm = ({ fetchJSON }) => {
 
   const [startQuarter, setStartQuarter] = useState(localStartQuarter || quarters[0].yyyyq);
   const [endQuarter, setEndQuarter] = useState(localEndQuarter || quarters[0].yyyyq);
-  //const [subject, setSubject] = useState(localSubject || {});
-  //const [courseNumber, setCourseNumber] = useState(localCourseNumber || "");
-  //const [courseSuf, setCourseSuf] = useState("");
   const [instructor, setInstructor] = useState(localInstructor || "");
     
   const handleSubmit = (event) => {
     event.preventDefault();
-    //fetchJSON(event, { startQuarter, endQuarter, subject, courseNumber, courseSuf });
     fetchJSON(event, { startQuarter, endQuarter, instructor});
   };
-
-
-
 
   // Stryker disable all : Stryker is testing by changing the padding to 0. But this is simply a visual optimization as it makes it look better
   return (
