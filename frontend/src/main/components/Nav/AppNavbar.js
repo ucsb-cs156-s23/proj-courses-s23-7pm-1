a0e2352af83ff030b818834f9e4749fd053f0d96
Krishna Dhulipala <krishna@Laxmans-MacBook-Pro.local>
2023-06-06 21:37:38 -0700

    KD-added 'Last Updated' page to admin dropdown, as well as page content (filler for now), and tests

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -92,6 +92,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                     <NavDropdown.Item href="/admin/users" data-testid="appnavbar-admin-users">Users</NavDropdown.Item>
                     <NavDropdown.Item href="/admin/personalschedule" data-testid="appnavbar-admin-personalschedule">Personal Schedules</NavDropdown.Item>
                     <NavDropdown.Item href="/admin/loadsubjects" data-testid="appnavbar-admin-loadsubjects">Load Subjects</NavDropdown.Item>
+                    <NavDropdown.Item href="/admin/lastupdated" data-testid="appnavbar-admin-lastupdated">Last Updated</NavDropdown.Item>
                     <NavDropdown.Item href="/admin/jobs" data-testid="appnavbar-admin-jobs">Manage Jobs</NavDropdown.Item>
                   </NavDropdown>
                 )
